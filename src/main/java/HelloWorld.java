09fd25bfccafb817ad1473bb35cdf2e10e29e04d
mangroa <alan.mangroo@gmail.com>
2017-08-04 13:08:54 +1000

    remove feature

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!!");
-        System.out.println("Feature 2");
         System.out.println("Feature 3");
         System.out.println("Feature 4");
         System.out.println("Feature 5");
