c59712cb5e4a6ea4e17e55642abab38f86f45693
mangroa <alan.mangroo@gmail.com>
2017-08-04 11:09:50 +1000

    Added feature 2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!!");
         System.out.println("Feature 1");
+        System.out.println("Feature 2");
         System.out.println("Time:" + System.currentTimeMillis());
     }
 
